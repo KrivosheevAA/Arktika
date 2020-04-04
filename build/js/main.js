47a9703c16443c8eda73b266b8407aed82391de0
Anton Krivosheev <krivosheev777@gmail.com>
2020-04-04 21:04:44 +0300

    верстка подавала дестоп

--- build/js/main.js
+++ build/js/main.js
@@ -1,8 +1,10 @@
-  var header = document.querySelector('.page-header');
-  var navToggle = header.querySelector('.main-nav__toggle');
+'use strict';
+
+var header = document.querySelector('.page-header');
+var navToggle = header.querySelector('.main-nav__toggle');
 
 document.body.classList.remove('site-body--nojs');
 
-  navToggle.addEventListener('click', function() {
-    header.classList.toggle('page-header--opened');
-  });
+navToggle.addEventListener('click', function () {
+  header.classList.toggle('page-header--opened');
+});
